4e15632a002834db7b0aad66c8e83a84179a14a1
fasill <fasilhawultie19@gmail.com>
2024-09-05 11:01:42 +0300

    about page added

--- components/asParents/Page.tsx
+++ components/asParents/Page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
     <div className="p-8 md:p-[5rem] h-[50vh] flex items-center justify-center">
       <div className="text-center md:text-left ">
         <FaQuoteLeft size={40} className="text-secondary mx-auto md:mx-0" />
-        <p className="text-lg md:text-[2rem] mt-4 whitespace-pre-line">
+        <p className="text-lg md:text-[2rem] mt-4 whitespace-pre-line">  
         As parents ourselves, we know how quickly little ones grow up. That&apos;s why we launched LillDiary - to provide families with a safe, private, and beautifully designed space. You can use LillDiary to save photos, videos, written memories, and more in one organized online album.
         </p>
       </div>
---
+++ components/subComponents/WhyUs.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WhyUs = () => {
+  return (
+    <div className='whyUs relative h-[40lvh] flex items-center justify-center '>
+        <h1 className='text-white font-[700] text-[40px]'>Why LilDiary?</h1>
+    </div>
+  )
+}
+
+export default WhyUs
